3ea8fddf3fcbac1d84634710f03f148b3142df31
Tobias Aispuro <tobiasaispuro10@gmail.com>
2025-02-14 12:46:37 -0300

    email script

--- index.ts
+++ index.ts
@@ -1,51 +1,32 @@
 import { webkit } from 'playwright'
 import * as dotenv from 'dotenv';
+import { Resend } from 'resend';
 
-  dotenv.config();
+dotenv.config();
 
-  const browser = await webkit.launch()
-  const page = await browser.newPage()
+const browser = await webkit.launch()
+const page = await browser.newPage()
 
-  await page.goto('https://consbahiablanca.esteri.it/es/news/')
+await page.goto('https://consbahiablanca.esteri.it/es/news/')
 
-  const lastNew = await page.$eval('.card-body', (element) => {
-    const title = element.querySelector('h5')?.textContent || 'Título no disponible';
-    return  title ;
-  });
+const lastNew = await page.$eval('.card-body', (element) => {
+  const title = element.querySelector('h5')?.textContent || 'Título no disponible';
+  return  title ;
+});
 
-  if (lastNew.includes("servicio")) {
-    console.log('Se encontraron turnos disponibles')
+if (lastNew.includes("reconstrucción")) {   
+  console.log('Se encontraron turnos disponibles')
+     
+  const resend = new Resend(process.env.MJ_APIKEY_PRIVATE);
+  resend.emails.send({
+  from: 'onboarding@resend.dev',
+  to: '',
+  subject: 'HABILITICACIÓN DE TURNOS!',
+  html: '<p>Se encuentran habilitados los turnos servicio ciudadanía por reconstrucción!</p>'
+   
+ });
+};
 
-    await fetch('https://api.mailjet.com/v3.1/send', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Basic ${Buffer.from(`${process.env.MJ_APIKEY_PUBLIC}:${process.env.MJ_APIKEY_PRIVATE}`).toString('base64')}`
-        },
-        body: JSON.stringify({
-          Messages: [
-            {
-              From: {
-                Email: "",
-                Name: ""
-              },
-              To: [
-                {
-                  Email: "",
-                  Name: ""
-                }
-              ],
-              Subject: "Hay turnos disponibles en la embajada!",
-              TextPart: "Esta habilitado la reservacion de turnos por reconstrucción",
-              HTMLPart: "<h3>Esta habilitado la reservacion de turnos por reconstrucción</h3><br />May the delivery force be with you!"
-            }
-          ]
-        })
-      });
-    };
- 
-  console.log(await lastNew)
-
-  await browser.close()
+await browser.close()
 
 
